2ac458af123d2218d0e96663f6e5f19025c6faa3
vnebra4niy <119805901+vnebra4niy@users.noreply.github.com>
2024-03-31 18:01:01 +0200

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './app.less'
+import { useDispatch, useSelector } from 'react-redux';
+import { setCount } from '../reducers/reposReducer';
 const App = () => {
+    const dispatch=useDispatch()
+    const count=useSelector(state => state.repos.count)
+    function onCountClick() {
+        dispatch(setCount(5))
+    }
+
     return (
         <div className="app">
-            React is working!
+            <button onClick={()=>onCountClick()}>Count</button>
+            <div>{count}</div>
         </div>
     );
 };
