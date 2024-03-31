688436be27cbe64e230dc5d701138cde2d3b5bc7
vnebra4niy <119805901+vnebra4niy@users.noreply.github.com>
2024-03-31 15:12:14 +0200

    3

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,19 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: ["@babel/polyfill", "./src/index.jsx"],
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "[name].[hash].js"       
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[hash].js"
     },
     devServer: {
         port: 3000
     },
     plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/index.html'
-        }),
+        new HTMLWebpackPlugin({template: "./src/index.html"}),
         new CleanWebpackPlugin()
     ],
     module: {
@@ -25,9 +23,29 @@ module.exports = {
                 use: ["style-loader", "css-loader", "less-loader"]
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                test: /\.(jpg|jpeg|png|svg)/,
                 use: ['file-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader:"babel-loader",
+                    options: {
+                        presets:["@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader:"babel-loader",
+                    options: {
+                        presets:["@babel/preset-react", "@babel/preset-env"]
+                    }
+                }
             }
         ]
     }
-};
+}
